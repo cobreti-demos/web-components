b509e3b6540ef9e371af77d0432ee18657e94034
Danny T <cobreti_@hotmail.com>
2023-08-31 19:09:05 -0400

    added source for appdemo1

---
+++ Apps/appdemo1/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import sassDts from 'vite-plugin-sass-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8000,
+    host: "0.0.0.0"
+  },
+  plugins: [
+      sassDts(),
+      react()
+  ]
+})
