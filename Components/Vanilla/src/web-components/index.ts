286de0117ceeb62e4c05378892fe64ff1510c6f6
Danny T <cobreti_@hotmail.com>
2023-09-15 10:10:26 -0400

    removed spinbox

--- Components/Vanilla/src/web-components/index.ts
+++ Components/Vanilla/src/web-components/index.ts
@@ -1,7 +1,5 @@
-import Spinbox from "./spinbox/spinbox";
 import AddressForm from './address-form/address-form';
 
-customElements.define('test-spinbox', Spinbox);
 customElements.define('test-address-form', AddressForm);
 
 
@@ -11,5 +9,4 @@ export interface WebComponentDetails {
 
 export const webComponents = new Map<string, WebComponentDetails>();
 
-webComponents.set('test-spinbox', {});
 webComponents.set('test-address-form', {});
