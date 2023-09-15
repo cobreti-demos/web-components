560d34e0dff60f8042c8c0a46a78d6acfaef35ee
Danny T <cobreti_@hotmail.com>
2023-09-15 11:19:11 -0400

    added event listeners

--- Components/Vanilla/src/web-components/address-form/address-form.ts
+++ Components/Vanilla/src/web-components/address-form/address-form.ts
@@ -1,3 +1,4 @@
+import { debounceTime, fromEvent } from 'rxjs';
 import template from './address-form.html?raw';
 import styles from './address-form.scss?inline';
 
@@ -18,5 +19,38 @@ export default class AddressForm extends HTMLElement {
         this._shadowRoot = this.attachShadow({mode: 'open'});
         this._shadowRoot.appendChild(styleNode);
         this._shadowRoot.appendChild(templateNode.content.cloneNode(true));
+
+        const postalCodeElm = this._shadowRoot.getElementById('postal-code') as HTMLInputElement;
+        if (postalCodeElm) {
+            fromEvent(postalCodeElm, 'input')
+                .pipe(debounceTime(500))
+                .subscribe(ev => this.onPostalCodeChanged(postalCodeElm, ev));
+        }
+
+        const addressElm = this._shadowRoot.getElementById('address') as HTMLInputElement;
+        if (addressElm) {
+            fromEvent(addressElm, 'input')
+                .pipe(debounceTime(500))
+                .subscribe(ev => this.onAddressChanged(addressElm, ev));
+        }
+
+        const cityElm = this._shadowRoot.getElementById('city') as HTMLInputElement;
+        if (cityElm) {
+            fromEvent(cityElm, 'input')
+                .pipe(debounceTime(500))
+                .subscribe(ev => this.onCityChanged(cityElm, ev));
+        }
+    }
+
+    onPostalCodeChanged(elm: HTMLInputElement, ev: Event) {
+        console.log(elm.value);
+    }
+
+    onAddressChanged(elm: HTMLInputElement, ev: Event) {
+        console.log(elm.value);
+    }
+
+    onCityChanged(elm: HTMLInputElement, ev: Event) {
+        console.log(elm.value);
     }
 }
