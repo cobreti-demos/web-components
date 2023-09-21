6f1fb07ef183cdcbbaa343e0812152d4137dec8f
Danny T <cobreti_@hotmail.com>
2023-09-20 21:30:19 -0400

    added different config for dev and build

---
+++ Components/Vanilla/vite.build.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import sassDts from 'vite-plugin-sass-dts'
+import path from 'node:path';
+
+export default defineConfig({
+   server: {
+    port: 8001,
+    host: "0.0.0.0"
+   },
+   plugins: [
+    sassDts(),
+   ],
+   resolve: {
+      alias: {
+         '@interfaces': path.resolve(__dirname, './src/app/interfaces'),
+         '@web-components': path.resolve(__dirname, './src/web-components')
+      }
+   }
+});
