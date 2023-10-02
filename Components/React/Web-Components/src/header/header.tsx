2ded8f23249f0b30e6716db3bd5469cd96e410f8
Danny T <cobreti_@hotmail.com>
2023-10-01 21:18:36 -0400

    some cleanup

--- Components/React/Web-Components/src/header/header.tsx
+++ Components/React/Web-Components/src/header/header.tsx
@@ -1,8 +1,6 @@
 import styles from './header.scss?inline';
 import r2wc from "@r2wc/react-to-web-component";
 
-export { styles };
-
 export function Header () {
     return (
         <>
@@ -23,14 +21,9 @@ const oldConnectedCallback = WCHeader.prototype.connectedCallback;
 WCHeader.prototype.connectedCallback = function() {
     oldConnectedCallback.apply(this);
 
-    let styleElm = this.shadowRoot.getElementById('header-style');
-    if (!styleElm) {
-        styleElm = document.createElement('style');
-        styleElm.id = 'header-style';
-        styleElm.textContent = styles;
-        this.shadowRoot.appendChild(styleElm);
-    }
+    const styleElm = document.createElement('style');
+    styleElm.textContent = styles;
+    this.shadowRoot.appendChild(styleElm);
 }
 
-console.log('defining custom element');
 customElements.define('test-header', WCHeader);
