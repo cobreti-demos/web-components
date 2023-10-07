bc00daded227092e7991dd7de9a998991257ab28
Danny T <cobreti_@hotmail.com>
2023-10-06 21:04:39 -0400

    listen to event in the React Component

--- Apps/appdemo1/src/App.tsx
+++ Apps/appdemo1/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 import {WebComponentLoader} from "./web-component-loader.ts";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 declare global {
     namespace JSX {
@@ -15,14 +15,33 @@ const webComponentLoader = new WebComponentLoader('web-components.json');
 
 function App() {
 
+    const testHeaderRef = useRef<HTMLElement | null>(null);
+
+    const onLoginEvHandler = (ev: Event) => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        console.log('on-login event', ev);
+    }
+
     useEffect(() => {
         webComponentLoader.loadWebComponents();
     }, [webComponentLoader]);
 
+    useEffect(() => {
+        const htmlElm = testHeaderRef.current;
+        console.log('useEffect - register event handler', testHeaderRef);
+        htmlElm?.addEventListener('on-login', onLoginEvHandler);
+
+        return () => {
+            console.log('useEffect cleanup event handler');
+            htmlElm?.removeEventListener('on-login', onLoginEvHandler);
+        }
+    }, [testHeaderRef]);
+
     return (
         <div className="container">
             <div className="header">
-                <test-header></test-header>
+                <test-header ref={testHeaderRef}></test-header>
             </div>
             <div className="content">
                 <div className="form">
