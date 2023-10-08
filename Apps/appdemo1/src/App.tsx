34a406d7f53aa62c4b5ec71f41fb2c672896c1f7
Danny T <cobreti_@hotmail.com>
2023-10-07 22:03:35 -0400

    fixed build added Header React component

--- Apps/appdemo1/src/App.tsx
+++ Apps/appdemo1/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import {WebComponentLoader} from "./web-component-loader.ts";
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
+import Header from "./header/header.tsx";
 
 declare global {
     namespace JSX {
@@ -15,33 +16,14 @@ const webComponentLoader = new WebComponentLoader('web-components.json');
 
 function App() {
 
-    const testHeaderRef = useRef<HTMLElement | null>(null);
-
-    const onLoginEvHandler = (ev: Event) => {
-        ev.preventDefault();
-        ev.stopPropagation();
-        console.log('on-login event', ev);
-    }
-
     useEffect(() => {
         webComponentLoader.loadWebComponents();
     }, [webComponentLoader]);
 
-    useEffect(() => {
-        const htmlElm = testHeaderRef.current;
-        console.log('useEffect - register event handler', testHeaderRef);
-        htmlElm?.addEventListener('on-login', onLoginEvHandler);
-
-        return () => {
-            console.log('useEffect cleanup event handler');
-            htmlElm?.removeEventListener('on-login', onLoginEvHandler);
-        }
-    }, [testHeaderRef]);
-
     return (
         <div className="container">
             <div className="header">
-                <test-header ref={testHeaderRef}></test-header>
+                <Header></Header>
             </div>
             <div className="content">
                 <div className="form">
---
+++ Apps/appdemo1/src/header/header.tsx
@@ -0,0 +1,28 @@
+import {useEffect, useRef} from "react";
+
+export default function Header() {
+    const testHeaderRef = useRef<HTMLElement | null>(null);
+
+    const onLoginEvHandler = (ev: Event) => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        console.log('on-login event', ev);
+    }
+
+    useEffect(() => {
+        const htmlElm = testHeaderRef.current;
+        console.log('useEffect - register event handler', testHeaderRef);
+        htmlElm?.addEventListener('on-login', onLoginEvHandler);
+
+        return () => {
+            console.log('useEffect cleanup event handler');
+            htmlElm?.removeEventListener('on-login', onLoginEvHandler);
+        }
+    }, [testHeaderRef]);
+
+    return (
+        <>
+            <test-header ref={testHeaderRef}></test-header>
+        </>
+    )
+}
